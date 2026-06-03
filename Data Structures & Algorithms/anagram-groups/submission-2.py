cd2b451cfce3843d7b46ee791785bc275d8eb205
kethavathsaiyashwanth-dev <kethavathsaiyashwanth@gmail.com>
2026-06-04 02:02:06 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+            hash_map = {}
+            for s in strs:
+                key_str = ''.join(sorted(s))
+                if key_str not in hash_map:
+                    hash_map[key_str] = []
+                hash_map[key_str].append(s)
+
+            res = []
+            for key_str in hash_map:
+                res.append(hash_map[key_str])
+            return res
